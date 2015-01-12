cc8a1cad3b7e7b66acc9794c7d73e38779995b1d
Luis Flores <floresl@outlook.com>
2015-01-12 11:32:07 -0600

    Updated README

--- FitPetv1/FitPetv1.ino
+++ FitPetv1/FitPetv1.ino
@@ -1,23 +1,32 @@
 /*
-*FitPet v0.4: Holiday Edition
+*FitPet v0.5: Holiday Edition
  
-* TODO: 
- -Integrate Accelerometer, RTC, Bluetooth
- -Write Menus
- -Decide on memory locations on EEPROM for steps and other status
+* TODO:
+- Integrate Accelerometer, RTC
+- Write menus and create UI (Eddy)
+- Decide on memory locations on EEPROM for steps and other status
+- Integrate second Bluetooth module
+- Enable inter-pet communication
 
  More to be added.
 
 *HISTORY:
+1/12/2014 0.5
+- Integrated Bluetooth Serial Module. Works great from smartphone, not so much via PC.
+- Updated README.
+
 1/3/2015 0.4
-Added more comments to code. Finally implemented animations from Flash memory. Massive speed increases. Smooth animations now possible.
-Created loading animations. Working on tools to automate animation creation.
+- Added more comments to code. Finally implemented drawing sprites from Flash memory. **Massive speed increases.Smooth animations now possible.**
+- Created loading and sample pet animations. Working on tools to automate animation creation.
+- Integrated EEPROM and wrote driver code for it.
 
 12/26/14 0.3
-Added comments to code. Integrated ClearBMP.
+- **Refactored entire code base. Project now requires Visual Studio 2013, Arduino 1.5.x and the Visual Micro plugin.**
+- Added comments to code.
+- Integrated ClearBMP and many other library functions.
 */
 
-#include <Adafruit_GFX\Adafruit_GFX.h>
+#include <Adafruit_GFX_Library\Adafruit_GFX.h>
 #include <TFT_S6D02A1\TFT_S6D02A1.h>
 #include <SPI.h>
 #include <SD.h>
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-FitPet
+FitPet v0.5: Back to School Edition
 ======
 
 Description
@@ -8,7 +8,36 @@ Senior Design Project. Wearable pet that depends on your health to keep it healt
 
 TODO:
 =====
+###Overall:
+- Decide on what exactly the pets will be. (Pokemon sprites? Original, customizable sprites? Animated?)
 
-EVERYTHING ;_;
+###Software:
+- Integrate Accelerometer, RTC
+- Write menus and create UI (Eddy)
+- Decide on memory locations on EEPROM for steps and other status
+- Integrate second Bluetooth module
+- Enable inter-pet communication
 
-eddy was here**
+###Hardware:
+- Create updated schematics
+- Begin planning final Shield layout
+- Begin running the numbers for power consumption so we can order the battery
+- Figure out what extra parts are needed so we can place an order
+
+More to be added.
+
+HISTORY:
+=====
+1/12/2014 0.5
+- Integrated Bluetooth Serial Module. Works great from smartphone, not so much via PC.
+- Updated README.
+
+1/3/2015 0.4
+- Added more comments to code. Finally implemented drawing sprites from Flash memory. **Massive speed increases.Smooth animations now possible.**
+- Created loading and sample pet animations. Working on tools to automate animation creation.
+- Integrated EEPROM and wrote driver code for it.
+
+12/26/14 0.3
+- **Refactored entire code base. Project now requires Visual Studio 2013, Arduino 1.5.x and the Visual Micro plugin.**
+- Added comments to code. 
+- Integrated ClearBMP and many other library functions.
